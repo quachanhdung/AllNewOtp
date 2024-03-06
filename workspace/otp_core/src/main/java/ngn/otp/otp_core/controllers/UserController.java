df03b9c1c26b00d41c68badd4c44801d713a5042
quachanhdung <quachanhdung@localhost>
2024-03-06 20:12:36 +0700

    update 6/3/2024 20:12

--- workspace/otp_core/src/main/java/ngn/otp/otp_core/controllers/UserController.java
+++ workspace/otp_core/src/main/java/ngn/otp/otp_core/controllers/UserController.java
@@ -7,6 +7,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -25,40 +26,95 @@ public class UserController {
 	private UserService userService;
 	PropUtil prop;
 	private int privateKeyLength = 32;
-	
+
 	public UserController(UserService userService) {
 		this.userService=userService;
 		this.prop = ApplicationContextProvider.getContext().getBean(PropUtil.class);
-		
+
 		try {
 			this.privateKeyLength = Integer.parseInt(prop.get("privatekey.length"));
 		} catch (NumberFormatException e) {
 			this.privateKeyLength=32;
 		}
 	}
-	
+
 	@GetMapping("/sayHello")
 	Map<String, Object> sayHello(){
 		logger.info("/user/sayHello");
 		return CommonUtil.createResult(200, "Success", "hello, this is user controller");
 	}
-	
+
 	@PostMapping("/add")
-	Map<String,Object> register(@RequestBody Map<String, Object> requestBody) {
+	Map<String,Object> add(@RequestBody Map<String, Object> requestBody) {
+		String userId, email=null, phone=null, fullName=null;
+		try {
+			userId = requestBody.get("userId").toString();
+		}catch(Exception e) {
+			return CommonUtil.createResult(400, "userId is required", null);
+		}
+		//email
+		try {
+			email=requestBody.get("email").toString().trim();
+			if(CommonUtil.isValidEmailAddress(email)==false) {
+				email=null;
+			}
+		}catch(Exception e) {};
+		//phone
+		try {
+			phone=requestBody.get("phone").toString().trim();
+			if(CommonUtil.isValidPhoneNumber(phone)==false) {
+				phone=null;
+			}
+		}catch(Exception e) {}
+		//fullName
+		try {
+			fullName=requestBody.get("fullName").toString().trim();
+		}catch(Exception e) {}
+
+		try {
+			UserModel userModel=userService.findById(userId);
+			if(userModel==null) {
+				userModel=new UserModel();
+				userModel.setUserId(userId);
+				userModel.setEmail(email);
+				userModel.setFullName(fullName);
+				userModel.setPrivateKey(TOTPUtil.generatePrivateKey(this.privateKeyLength));
+				userModel.setActiveCode(TOTPUtil.generateRandomNumberString());
+				userModel.setEnable(true);
+				userModel.setDateCreated(new Date());
+				userModel.setOrganization("Peoples");
+				userService.save(userModel);
+				return CommonUtil.createResult(200, "OK", userModel);
+			}else {
+				return CommonUtil.createResult(409, "User exist", null);
+			}
+		} catch (Exception e) {
+			return CommonUtil.createResult(409,"duplicate entry: email or phone",null);
+		}
+	}
+
+	@PutMapping("/update")
+	Map<String,Object> update(@RequestBody Map<String, Object> requestBody) {
 		String userId, email="", phone="";
 		try {
-			 userId = requestBody.get("userId").toString();
+			userId = requestBody.get("userId").toString();
 		}catch(Exception e) {
 			return CommonUtil.createResult(999, e.toString(), null);
 		}
-		
+
 		try {
-			 email=requestBody.get("email").toString();
-			 phone=requestBody.get("phone").toString();
+			email=requestBody.get("email").toString();
+			phone=requestBody.get("phone").toString();
+			if(CommonUtil.isValidEmailAddress(email)==false) {
+				email="";
+			}
+			if(CommonUtil.isValidPhoneNumber(phone)==false) {
+				phone="";
+			}
 		}catch(Exception e) {
-			
+
 		}
-		
+
 		UserModel userModel=userService.findById(userId);
 		if(userModel==null) {
 			userModel=new UserModel();
--- workspace/otp_core/src/main/java/ngn/otp/otp_core/utils/CommonUtil.java
+++ workspace/otp_core/src/main/java/ngn/otp/otp_core/utils/CommonUtil.java
@@ -2,6 +2,8 @@ package ngn.otp.otp_core.utils;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class CommonUtil {
 	public static Map<String, Object> createResult(int status, String message, Object data){
@@ -11,5 +13,21 @@ public class CommonUtil {
 		map.put("data", data);
 		return map;
 	}
+	
+	public static boolean isValidEmailAddress(String emailAddress) {
+		String emailPattern = "^[\\w!#$%&’*+/=?`{|}~^-]+(?:\\.[\\w!#$%&’*+/=?`{|}~^-]+)*@(?:[a-zA-Z0-9-]+\\.)+[a-zA-Z]{2,6}$";
+        Pattern p = Pattern.compile(emailPattern);
+        Matcher m = p.matcher(emailAddress);
+        return m.matches();
+	}
+	
+	public static boolean isValidPhoneNumber(String phoneNumber) {
+		 String pattern = "^\\d{10}$";
+	        // Create a Pattern object
+	        Pattern r = Pattern.compile(pattern);
+	        // Create a Matcher object
+	        Matcher m = r.matcher(phoneNumber);
+	      return m.matches();
+    }
 
 }
