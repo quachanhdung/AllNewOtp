71743cf7222523ecb46bc7c8ded4e08b13fd5dd7
quachanhdung <quachanhdung@dzungs-mbp>
2024-03-16 10:30:09 +0700

    update 16/03/2024 10:30

--- workspace/otp_core/src/main/java/ngn/otp/otp_core/services/LdapServerService.java
+++ workspace/otp_core/src/main/java/ngn/otp/otp_core/services/LdapServerService.java
@@ -61,7 +61,7 @@ public class LdapServerService {
 			if(model.getServerType()==1) {
 				result = ldapAuthenticate(model);
 			}else {
-//				 result= ActiveDirectoryService.authen(model);
+				 result= adAuthenticate(model);
 			}
 			if(result) return result; 
 			
@@ -75,17 +75,27 @@ public class LdapServerService {
 		String userName = model.getPrincipal();
 		String password = model.getCredential();
 		userName=userName.split("@")[0];
-		logger.info(userName);
-		logger.info("ldapUrl: "+model.getServerUrl());
-		logger.info("searchBase: "+model.getSearchBase());
-		logger.info("userBase: "+model.getBinddn()+"=" + userName + "," + model.getSearchBase());
+		if (userName.contains("\\")){
+			userName = userName.substring(userName.indexOf("\\")+1);
+		}
+		
+		String PROVIDER_URL = model.getServerUrl();
+	    String SECURITY_AUTHENTICATION = "simple";
+	    String SECURITY_PRINCIPAL = model.getBinddn()+"=" + userName + "," + model.getSearchBase();
+	    String SECURITY_CREDENTIALS = password;
+	    
+	    logger.info("PROVIDER_URL: "+PROVIDER_URL);
+	    logger.info("SECURITY_AUTHENTICATION: "+SECURITY_AUTHENTICATION);
+	    logger.info("SECURITY_PRINCIPAL: "+SECURITY_PRINCIPAL);
+	    logger.info("SECURITY_CREDENTIALS: "+SECURITY_CREDENTIALS);
+		
 		Hashtable<String, String> env = new Hashtable<String, String>();
 	    env.put("com.sun.jndi.ldap.connect.timeout", "2000");
 	    env.put(Context.INITIAL_CONTEXT_FACTORY, "com.sun.jndi.ldap.LdapCtxFactory");
-	    env.put(Context.PROVIDER_URL, model.getServerUrl());
-	    env.put(Context.SECURITY_AUTHENTICATION, "simple");
-	    env.put(Context.SECURITY_PRINCIPAL, model.getBinddn()+"=" + userName + "," + model.getSearchBase());
-	    env.put(Context.SECURITY_CREDENTIALS, password);
+	    env.put(Context.PROVIDER_URL, PROVIDER_URL);
+	    env.put(Context.SECURITY_AUTHENTICATION, SECURITY_AUTHENTICATION);
+	    env.put(Context.SECURITY_PRINCIPAL, SECURITY_PRINCIPAL);
+	    env.put(Context.SECURITY_CREDENTIALS, SECURITY_CREDENTIALS);
 	    DirContext ctx = null;
 	    boolean returnValue=false;
 	    try {
@@ -100,6 +110,50 @@ public class LdapServerService {
 	        }
 	    }
 	    return returnValue;
+	}
+	
+	public static boolean adAuthenticate(LdapServerModel model) {
+		logger.info("=====LdapServerService::adAuthenticate()=====");
+		String userName = model.getPrincipal().trim();
+		String password = model.getCredential().trim();
+		String domainName=model.getDomainName();
+		userName = userName.split("@")[0];
+		if (userName.contains("\\")){
+			userName = userName.substring(userName.indexOf("\\")+1);
+		}
+		
+		
+	    String PROVIDER_URL = model.getServerUrl();
+	    String SECURITY_AUTHENTICATION = "simple";
+	    String SECURITY_PRINCIPAL = userName + "@" + domainName;
+	    String SECURITY_CREDENTIALS = password;
+	   
+	  
+	   
+	    logger.info("PROVIDER_URL: "+PROVIDER_URL);
+		logger.info("SECURITY_AUTHENTICATION: "+SECURITY_AUTHENTICATION);
+		logger.info("SECURITY_PRINCIPAL: "+SECURITY_PRINCIPAL);
+		logger.info("SECURITY_CREDENTIALS: "+SECURITY_CREDENTIALS);
+		
+	    
+	    Hashtable<String, String> env = new Hashtable<String, String>();
+	    env.put("com.sun.jndi.ldap.connect.timeout", "2000");
+	    env.put(Context.INITIAL_CONTEXT_FACTORY, "com.sun.jndi.ldap.LdapCtxFactory");
+	    env.put(Context.PROVIDER_URL, PROVIDER_URL);
+	    env.put(Context.SECURITY_AUTHENTICATION, SECURITY_AUTHENTICATION);
+	    env.put(Context.SECURITY_PRINCIPAL, SECURITY_PRINCIPAL);
+	    env.put(Context.SECURITY_CREDENTIALS, SECURITY_CREDENTIALS);
+	    DirContext ctx = null;
+	    try {
+	        ctx = new InitialDirContext(env);
+	        return true; // authentication succeeded
+	    } catch (NamingException e) {
+	        return false; // authentication failed
+	    } finally {
+	        if (ctx != null) {
+	            try { ctx.close(); } catch (NamingException e) {}
+	        }
+	    }
 	}
 	
 	
